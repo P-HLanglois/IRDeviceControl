07afd51b61ff92fdb4dddfef62fddf440c84b77d
Vash94350 <michael.pean.mp@gmail.com>
2019-05-02 15:54:45 +0200

    to verify code throw :: SenderReceiver + To ShutDown -> TurnOn VideoProjector

---
+++ SenderReceiver/SenderReceiver.ino
@@ -0,0 +1,53 @@
+#ifndef UNIT_TEST
+#include <Arduino.h>
+#endif
+#include <IRremoteESP8266.h>
+#include <IRsend.h>
+#include <IRremoteESP8266.h>
+#include <IRrecv.h>
+#include <IRutils.h>
+
+const int RECV_PIN = D4;
+IRrecv irrecv(RECV_PIN);
+decode_results results;
+
+const uint16_t kIrLed = 0;  // ESP8266 GPIO pin to use. Recommended: 0 (D3).
+const int signalOnOffVideoProj = 0x00C1AA09F6;
+const int signalOnOffTV = 0x4C;
+
+IRsend irsend(kIrLed);  // Set the GPIO to be used to sending the message.
+
+void setup(){
+  irsend.begin();
+  Serial.begin(115200, SERIAL_8N1, SERIAL_TX_ONLY);
+  irrecv.enableIRIn();
+}
+
+void loop(){
+  if (irrecv.decode(&results)){
+        serialPrintUint64(results.value, HEX);
+        switch (results.decode_type){
+            case NEC: Serial.println(" NEC"); break ;
+            case SONY: Serial.println(" SONY"); break ;
+            case RC5: Serial.println(" RC5"); break ;
+            case RC6: Serial.println(" RC6"); break ;
+            case DISH: Serial.println(" DISH"); break ;
+            case SHARP: Serial.println(" SHARP"); break ;
+            case JVC: Serial.println(" JVC"); break ;
+            case SANYO: Serial.println(" SANYO"); break ;
+            case MITSUBISHI: Serial.println(" MITSUBISHI"); break ;
+            case SAMSUNG: Serial.println(" SAMSUNG"); break ;
+            case LG: Serial.println(" LG"); break ;
+            case WHYNTER: Serial.println(" WHYNTER"); break ;
+            case AIWA_RC_T501: Serial.println(" AIWA_RC_T501"); break ;
+            case PANASONIC: Serial.println(" PANASONIC"); break ;
+            case DENON: Serial.println(" DENON"); break ;
+          default:
+            case UNKNOWN: Serial.println(" UNKNOWN"); break ;
+          }
+        irrecv.resume();
+ }
+// irsend.sendNEC(signalOnOffVideoProj);
+ irsend.sendRC5(signalOnOffTV);
+ delay(3000);
+}
